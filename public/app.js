e5f86f86301ec10df0023dae1ebb5993ac1f6767
roosevelt <rooseveltdossantos@hotmail.com>
2016-07-27 08:56:21 -0300

    Estrutura para composição dos problemas a serem resolvidos.

--- public/app.js
+++ public/app.js
@@ -2,9 +2,46 @@
 
 var learnjs = {}; 
 
-learnjs.problemView = function(problemNumber) {
-	var title = 'Problem #' + problemNumber + ' Coming soon!';
-	return $('<div class="problem-view">').text(title);
+learnjs.problems = [
+	{
+		description: "What is truth?",
+		code: "function problem () { return __; }"
+	},
+	{
+		description: "Simple Math",
+		code: "function problem () { return 42 === 6 * __; }"
+	}
+];
+
+learnjs.problemView = function(data) {
+	var problemNumber = parseInt(data, 10);
+	var view = $('.templates .problem-view').clone();
+
+	var problemData = learnjs.problems[problemNumber -1];
+	var resultFlash = view.find('.result');
+
+	function checkAnswer() {
+		var answer = view.find('.answer').val();
+		var test = problemData.code.replace('__', answer) + '; problem();';
+		return eval(test);
+	}
+
+	function checkAnswerClick()
+	{
+		if (checkAnswer())
+		{
+			learnjs.flashElement(resultFlash, 'Correct!');
+		} else {
+			learnjs.flashElement(resultFlash, 'Incorrect!');
+		}
+		return false;
+	}
+
+	view.find('.check-btn').click(checkAnswerClick);
+	view.find('.title').text('Problem #' + problemNumber) ;
+	learnjs.applyObject(problemData, view);
+
+	return view;
 }
 
 learnjs.showView = function(hash){
@@ -26,4 +63,18 @@ learnjs.appOnReady = function(){
 		learnjs.showView(window.location.hash);
 	}
 	learnjs.showView(window.location.hash);
+}
+
+learnjs.applyObject = function(obj, elem){
+	for (var key in obj)
+	{
+		elem.find('[data-name="' + key + '"]').text(obj[key]);
+	}
+}
+
+learnjs.flashElement = function(elem, content){
+	elem.fadeOut('fast', function (){
+		elem.html(content);
+		elem.fadeIn();
+	});
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -13,11 +13,12 @@ describe('LearnJS', function(){
 		learnjs.showView('#problem-42');
 		expect(learnjs.problemView).toHaveBeenCalledWith('42');
 	});
-	describe('problem view', function() {
+	describe('problem view', function() {		
+
 		it('has a title that includes the problem number', function() {
 			var view = learnjs.problemView('1');
-			expect(view.text()).toEqual('Problem #1 Coming soon!');
-		})
+			expect(view.find('.title').text()).toEqual('Problem #1');
+		});
 	});
 	it('invokes the router when loaded', function(){
 		spyOn(learnjs, 'showView');
@@ -30,4 +31,18 @@ describe('LearnJS', function(){
 		$(window).trigger('hashchange');
 		expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
 	});
+	describe('answer section', function() {
+		it ('can check a correct answer by hitting a button', function() {
+			var view = learnjs.problemView('1');
+			view.find('.answer').val('true');
+			view.find('.check-btn').click();
+			expect(view.find('.result').text()).toEqual('Correct!');
+		});
+		it('rejects an incorrect answer', function() {
+			var view = learnjs.problemView('1');
+			view.find('.answer').val('false');
+			view.find('.check-btn').click();
+			expect(view.find('.result').text()).toEqual('Incorrect!');
+		})
+	});			
 })
