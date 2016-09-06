c7708ef95733e7090ff841a14d53fd5ae10b1da7
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2016-09-05 22:07:52 -0300

    Finalizando tratamento da entrada do usuário

--- public/app.js
+++ public/app.js
@@ -28,11 +28,12 @@ learnjs.problemView = function(data) {
 
 	function checkAnswerClick()
 	{
-		if (checkAnswer())
-		{
-			learnjs.flashElement(resultFlash, 'Correct!');
+		if (checkAnswer()){
+			var correctFlash = learnjs.template('correct-flash');
+			correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+			learnjs.flashElement(resultFlash, correctFlash);
 		} else {
-			learnjs.flashElement(resultFlash, 'Incorrect!');
+			learnjs.flashElement(resultFlash, 'Incorreto!');
 		}
 		return false;
 	}
@@ -77,4 +78,20 @@ learnjs.flashElement = function(elem, content){
 		elem.html(content);
 		elem.fadeIn();
 	});
+}
+
+learnjs.template = function(name){
+	return $('.templates .' + name).clone();
+}
+
+learnjs.buildCorrectFlash = function(problemNum){
+	var correctFlash = learnjs.template('correct-flash');
+	var link = correctFlash.find('a');
+	if (problemNum < learnjs.problems.length){
+		link.attr('href', '#problem-' + (problem + 1));		
+	} else {
+		link.attr('href', '');
+		link.text('Você Finalizou');
+	}
+	return correctFlash;
 }
